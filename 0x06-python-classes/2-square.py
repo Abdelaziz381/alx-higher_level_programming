32bdcd367cbdd8c9f4812e456d7512e2ed7950a6
AbdelazizElsawy <abdelaziz38elsawy@gmail.com>
2024-01-27 03:19:12 +0200

    2

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -4,7 +4,7 @@
 
 class Square():
     """Square Empty Class."""
-    def __init__(self, size):
+    def __init__(self, size=0):
         """Constructir.
         Args:
             size: side square length
