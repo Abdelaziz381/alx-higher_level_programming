fd615e0e5a9f16476fac9230dac1c047f372dc18
AbdelazizElsawy <abdelaziz38elsawy@gmail.com>
2024-01-27 03:12:42 +0200

    1

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""Square Class."""
+
+
+class Square():
+    """Square Empty Class."""
+    def __init__(self, size):
+        """Constructir.
+        Args:
+            size: side square length
+        """
+        self.__size = size
