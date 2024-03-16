b13635a32c01b29551d736ed874903a2d7d7a682
AbdelazizElsawy <abdelaziz38elsawy@gmail.com>
2024-03-17 00:23:35 +0200

    5

--- 0x0D-SQL_introduction/5-full_table.sql
+++ 0x0D-SQL_introduction/5-full_table.sql
@@ -1,4 +1,4 @@
 -- script that prints the full description of the table 
 -- first_table from the database hbtn_0c_0 in your MySQL server.
 
-show create table first_table;
+SELECT * FROM INFORMATION_SCHEMA.COLUMNS WHERE TABLE_NAME = 'first_table';
